b451ed5fc62e2c3f6b1cf03d3451d97252cb4881
Varun Prabhakar <varun.prabhakar@utexas.edu>
2021-10-16 12:01:35 -0500

    Add project files.

---
+++ DeleteEmptyDirectories/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DeleteEmptyDirectories
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFilepath = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(202, 13);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(78, 19);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFilepath
+            // 
+            this.txtFilepath.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtFilepath.Location = new System.Drawing.Point(12, 12);
+            this.txtFilepath.Name = "txtFilepath";
+            this.txtFilepath.Size = new System.Drawing.Size(169, 20);
+            this.txtFilepath.TabIndex = 3;
+            this.txtFilepath.Text = "Select the source directory";
+            this.txtFilepath.Click += new System.EventHandler(this.txtFilepath_Click);
+            this.txtFilepath.TextChanged += new System.EventHandler(this.txtFilepath_TextChanged);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(202, 48);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(81, 29);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 88);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilepath);
+            this.Name = "Form1";
+            this.Text = "Remove Empty Folders";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFilepath;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ DeleteEmptyDirectories/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+//written by vprabh01
+namespace DeleteEmptyDirectories
+{
+    public partial class Form1 : Form
+    {
+        String sourceD;
+        Boolean sourceChanged = false;
+        public Form1()
+        {
+            InitializeComponent();
+            btnBrowse.Select();
+            ToolTip dirInfo = new ToolTip();
+            dirInfo.ShowAlways = true;
+            dirInfo.SetToolTip(txtFilepath, "Select a directory to clear of empty subdirectories");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtFilepath.Text.ToString() == "")
+                {
+                    MessageBox.Show("Please select a directory");
+                    return;
+                }
+                DirectoryInfo di = new DirectoryInfo(sourceD);
+                if (MessageBox.Show("Delete all empty directories in '" + sourceD + "'?", "Continue?", MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+                    return;
+                deleteEmpty(di);
+                MessageBox.Show("Complete.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message.ToString());
+            }
+        }
+
+        private void deleteEmpty(DirectoryInfo sourceInfo)
+        {
+            try
+            {
+                DirectoryInfo di = sourceInfo;
+                DirectoryInfo[] allSub = di.GetDirectories();
+                foreach (DirectoryInfo subDir in allSub)
+                    deleteEmpty(subDir);
+                allSub = di.GetDirectories();
+                FileInfo[] allFiles = di.GetFiles();
+                if (allFiles.Length == 0 && allSub.Length == 0)
+                    di.Delete();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message.ToString());
+            }
+        }
+
+        private void txtFilepath_TextChanged(object sender, EventArgs e)
+        {
+            sourceD = txtFilepath.Text;
+            sourceChanged = true;
+        }
+
+        private void txtFilepath_Click(object sender, EventArgs e)
+        {
+            if (!sourceChanged)
+                txtFilepath.Clear();
+            txtFilepath.ForeColor = Color.Black;
+            sourceChanged = true;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dr = folderBrowserDialog1.ShowDialog();
+                if (dr == System.Windows.Forms.DialogResult.OK)
+                {
+                    txtFilepath.ForeColor = Color.Black;
+                    txtFilepath.Text = folderBrowserDialog1.SelectedPath;
+                    sourceChanged = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message.ToString());
+            }
+        }
+    }
+}
